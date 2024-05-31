2fa57293f58515930ec08d03f6363705e5b4f252
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-05-31 20:02:03 -0300

    controller

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,75 +3,61 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    private SeriesRepository $seriesRepository;
+
+    public function __construct(SeriesRepository $seriesRepository)
+    {
+        $this->seriesRepository = $seriesRepository;
+    }
 
     public function index()
     {
         return Series::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(SeriesFormRequest $request)
     {
-        //
+        $series = Series::created($request);
+        return response()->json($series, 200);
+    }
+
+    public function show(string $id)
+    {
+        $serie = Series::find($id)->with('seasons.episodes')->get();
+
+        if($serie == null){
+            return response()->json('Série não encontrada', 404);
+        }
+
+        return $serie;
     }
 
 
-    public function store(Request $request)
+    public function update(SeriesFormRequest $request, Series $serie)
     {
-        dd($request->all());
+        if($serie == null){
+            return response()->json('Série não encontrada', 404);
+        }
+        $serie->fill($request->all());
+        $serie->save();
+
+        return $serie;
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function destroy(string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if($id == null){
+            return response()->json('Série não encontrada', 404);
+        }
+        Series::destroy($id);
+        return response()->noContent();
     }
 }
